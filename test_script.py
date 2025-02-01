a22fa4921ee599944ea6a6d8ba0c9f0bb2c9cf65
Jada <jada.r.thibodeaux@gmail.com>
2025-02-01 12:57:14 -0600

    Fixed if/else syntax (silly wabbit, the else function wasn't aligned properly!)

--- test_script.py
+++ test_script.py
@@ -1,11 +1,13 @@
 from animal_shelter import AnimalShelter
-#create the object from the class
-shelter = AnimalShelter("myUserAdmin","trinidad1" )
-data = {"age_upon_outcome":"1.7 years","animal_type":"Cat"}
-if shelter.create(data): 
-    print("animal added")
-    else:
-        print ("Error")
-    
-# return the value of the created record 
-return shelter.data
+
+# Create the object from the class
+shelter = AnimalShelter("myUserAdmin", "trinidad1")
+data = {"age_upon_outcome": "1.7 years", "animal_type": "Cat"}
+
+if shelter.create(data):  # Ensure create() returns True/False
+    print("Animal added")
+else:  # Fix indentation
+    print("Error")
+
+# Print the value of the created record instead of using return
+print(shelter.data)
